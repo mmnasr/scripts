efeeef6a7dc41c3542c1f9f2a8c36ab9d3ced375
teamDSenthusiasts <a.nabibidhendi@gmail.com>
2017-10-08 11:46:44 -0700

    add tools folder, add virtual env setup script

---
+++ tools/python-venv-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "virtual environment name: $1"
+# first change directory to where the venv should be created
+# cd ~/python-venvs
+virtualenv $1
+source $1/bin/activate
+
+pip install jupyter
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install scikit-learn
+pip install scipy
+pip install theano
+pip install keras
+pip install tensorflow
+pip install statsmodels
+# pip install nltk
